9e3055959f3119f5bfbcf55a19596bf9d30291b4
Jacob Silver <jacobsilver2@mac.com>
2017-12-02 14:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(facts) {
+  var i = 0;
+  while ( i <= facts.length) {
+    facts[i] += "!!!";
+  }
   
-  
-  return facts
+  return facts;
 }
